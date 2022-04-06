e36d2abe0fb769cff44595fa0e1014e589b85858
davidyoon <qqpo12@naver.com>
2022-04-06 22:56:38 +0900

    test get post with regural expression

--- src/main.js
+++ src/main.js
@@ -19,10 +19,19 @@ const http = require('http')
  */
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-
-  res.statusCode = 200
-  res.end('Hello!')
+  if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end('List of posts')
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200
+    res.end('Some content of the post')
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found.')
+  }
 })
 
 const PORT = 4000
